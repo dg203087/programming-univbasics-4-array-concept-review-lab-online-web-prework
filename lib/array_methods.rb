46d1963fb4e0110545bd2f4242e3d0a7d569de0e
Deanna Kelley <deanna.georges@gmail.com>
2019-09-22 13:55:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,10 +10,13 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  x = array(0)
+  x = array[0]
   array.length.times do  |index|
-  if array[index] > x 
-  return array.max(element)
+    if array[index] > x 
+    x = array[index]
+  end
+end
+x
 end
 
 def find_min_value(array)
